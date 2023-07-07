7a79f6be0d97ed7262fc5b5f639cbc6e4bec1ddf
MortezaHagh <m.haghbeigi@gmail.com>
2023-07-07 12:49:46 +0200

    doc

--- LPAStar/checkForUpdate.m
+++ LPAStar/checkForUpdate.m
@@ -3,7 +3,6 @@ function [Open, RHS, Model] = checkForUpdate(Open, RHS, Model, G, t)
 % check for Map change
 for i=1:Model.NewObsts.count
     if t==Model.NewObsts.t(i)
-        % update model
         newObstNode = Model.NewObsts.nodeNumbers(i);
         Model.Predecessors{newObstNode,2} = Model.Predecessors{newObstNode,2}+inf;
         
